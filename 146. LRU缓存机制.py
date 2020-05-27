09ca178b8f3f31016e93d52aa1f5bcdced091ba6
Laura_Long <laura_long@Laura-LongdeMBP.lan>
2020-05-28 00:08:02 +0800

    新题目

---
+++ 146. LRU缓存机制.py
@@ -0,0 +1,22 @@
+from queue import deque
+
+
+q = deque()
+class LRUCache:
+    # Your LRUCache object will be instantiated and called as such:
+    # obj = LRUCache(capacity)
+    # param_1 = obj.get(key)
+    # obj.put(key,value)
+
+    def __init__(self, capacity: int):
+        self.q = Queue(capacity)
+
+
+
+
+    def get(self, key: int) -> int:
+
+
+    def put(self, key: int, value: int) -> None:
+
+
--- 5. 最长回文子串.py
+++ 5. 最长回文子串.py
@@ -4,13 +4,17 @@ class Solution:
             return ''
         length = len(s)
         result = dict()
+        sota_result = 0
         for outb in range(length):
+            if sota_result > length - outb:
+                break
             for inb in range(outb, length):
                 temps = s[outb:inb+1]
                 if temps == temps[::-1]:
                     result[temps] = len(temps)
+                    sota_result = max(sota_result, len(temps))
         return sorted(result.items(), key=lambda x: x[1], reverse=True)[0][0]
 
 
 s = Solution()
-print(s.longestPalindrome('aa'))
+print(s.longestPalindrome('aabbbabababbabababb'))
