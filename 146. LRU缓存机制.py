913988319ed410e80c1e3e71111d5bb363037879
Laura_Long <laura_long@Laura-LongdeMBP.lan>
2020-05-28 23:05:42 +0800

    LRU缓存机制

--- 146. LRU缓存机制.py
+++ 146. LRU缓存机制.py
@@ -1,7 +1,3 @@
-from queue import deque
-
-
-q = deque()
 class LRUCache:
     # Your LRUCache object will be instantiated and called as such:
     # obj = LRUCache(capacity)
@@ -9,14 +5,29 @@ class LRUCache:
     # obj.put(key,value)
 
     def __init__(self, capacity: int):
-        self.q = Queue(capacity)
-
-
-
+        self.queue = []
+        self.load = 0
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
-
+        for i in self.queue:
+            if i[0] == key:
+                retval = i[1]
+                self.queue.remove(i)
+                self.queue.insert(0, i)
+                return retval
+        return -1
 
     def put(self, key: int, value: int) -> None:
-
-
+        for i in self.queue:
+            if i[0] == key:
+                i[1] = value
+                self.queue.remove(i)
+                self.queue.insert(0, i)
+                return
+        if self.load == self.capacity:
+            self.queue.pop()
+            self.queue.insert(0, [key, value])
+        else:
+            self.queue.insert(0, [key, value])
+            self.load += 1
